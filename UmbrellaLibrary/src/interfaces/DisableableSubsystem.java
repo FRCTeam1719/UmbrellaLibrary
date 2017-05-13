e71086e3684d98a8eb427effac26ef2acc20553a
Kyle <chickengod37@gmail.com>
2017-05-13 19:14:57 -0400

    Removed copies of stuff from git

--- UmbrellaLibrary/src/interfaces/DisableableSubsystem.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface DisableableSubsystem {
-	public void disable();
-}
--- UmbrellaLibrary/src/interfaces/ISubsystem.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface ISubsystem extends DisableableSubsystem, LogicalSubsystem, Loggable {
-
-}
